91967202a14f874eed7e49f398e25ae92daca107
Sally <saule04.21@gmail.com>
2019-04-01 17:04:53 -0400

    Test commit

--- src/test/java/com/project/runners/CucumberRunner.java
+++ src/test/java/com/project/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features"},
         glue = {"com/project/"}
         , dryRun = false
-        , tags = "@Test"
+        , tags = ""
 )
 
 public class CucumberRunner {
